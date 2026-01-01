72ef39329ef86fd9e78b472ea66d8b9f0527815a
thatkunaaal <thatkunaal@gmail.com>
2026-01-01 06:56:58 +0530

    feat: added rate limiter and reverse-proxy for handling request effciently

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const { ServerConfig } = require("./config");
+const { rateLimit } = require("express-rate-limit");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 const apiRoutes = require("./routes");
 const cookieParser = require("cookie-parser");
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  limit: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(limiter);
+
+app.use(
+  "/bookingsService",
+  createProxyMiddleware({
+    target: "http://localhost:4000",
+    changeOrigin: true,
+    pathRewrite: { "^/bookingsService": "/" },
+  })
+);
 app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
