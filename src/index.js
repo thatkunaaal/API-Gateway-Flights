7eb415020a78b717e47ee993f52ec406a22a49ac
thatkunaaal <thatkunaal@gmail.com>
2026-01-05 01:31:41 +0530

    bug: move the express.json() later to prevent destreaming of req.body() for routes forwarding

--- src/index.js
+++ src/index.js
@@ -2,29 +2,31 @@ const express = require("express");
 const { ServerConfig } = require("./config");
 const { rateLimit } = require("express-rate-limit");
 const { createProxyMiddleware } = require("http-proxy-middleware");
-
-const { user } = require("./models");
-
 const app = express();
 const apiRoutes = require("./routes");
 const cookieParser = require("cookie-parser");
 const { UserMiddleware } = require("./middleware");
+
 const limiter = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 minutes
   limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
 });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(limiter);
 
 app.use(
   "/bookingService",
+  UserMiddleware.checkAuth,
   createProxyMiddleware({
     target: ServerConfig.BOOKING_SERVICE,
     changeOrigin: true,
     pathRewrite: { "^/bookingService": "/" },
+    on: {
+      proxyReq: (proxyReq, req, res) => {
+        proxyReq.setHeader("user", JSON.stringify(req.user.dataValues));
+      },
+    },
   })
 );
 
@@ -42,6 +44,9 @@ app.use(
     },
   })
 );
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", apiRoutes);
 
